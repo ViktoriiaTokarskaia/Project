b8c1974a3f4da27931362c59b544302e42f5cf35
nkjaernested/homework <natkja@gmail.com>
2019-09-08 13:59:43 +0300

    Update Person

--- src/main/java/models/Person.java
+++ src/main/java/models/Person.java
@@ -11,23 +11,7 @@ public class Person{
     Long id;
 
     String name;
-    String phone;
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
 
 
