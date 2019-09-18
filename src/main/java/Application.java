7fb076726773dbd7e6e285b06b000ab6f2ea1757
Vinod John <vinodjohn56@gmail.com>
2019-09-18 20:58:30 +0300

    Added comments

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -5,10 +5,15 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+
         AuthorDao authorDao = new AuthorDao();
+
+        //Add new author
         Author author = new Author();
         author.setName("Shakespere");
         authorDao.saveAuthor(author);
+
+        //List all authors
         List<Author> authors = authorDao.getAuthors();
         authors.forEach(a -> System.out.println(a.getName()));
     }
